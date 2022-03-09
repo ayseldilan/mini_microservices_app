17a1cdd072c944952b0d676e61b873d913849f1f
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-03-09 23:35:07 +0000

    final touches to comment section

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import PostCreate from './PostCreate';
-import PostList from './PostList';
+import React, {useState} from 'react'
+import PostCreate from './PostCreate'
+import PostList from './PostList'
 
-export default () => {
-    return <div>
-        <h1> Create Posts</h1>
+
+const App = () => {
+
+    return (
+    <div className='container'>
+        <h1>Create Post</h1>
         <PostCreate />
-        <hr />
-        <h1>Posts</h1>
-        <PostList />
+        <PostList></PostList>
     </div>
-};
+    
+  )
+}
+
+export default App
